30bfe6acb012579e38a046a64d84f1be174e9dc2
Adilson Sandoval <adilsonsdev@gmail.com>
2025-11-30 17:22:01 +0100

    move ordinalNumbers to its own module

--- tests/diffAmendmentsAgainstBaseline.spec.js
+++ tests/diffAmendmentsAgainstBaseline.spec.js
@@ -6,36 +6,7 @@ import { formatAmendmentData } from "../src/utils/formatAmendmentData.js";
 import { diffStructuredDoc } from "../src/utils/diffStructuredDoc.js";
 import { writeDiffReport } from "../src/utils/writeDiffReport.js";
 import config from "../config.js";
-
-const ordinalNumbers = new Map([
-  [1, "first"],
-  [2, "second"],
-  [3, "third"],
-  [4, "fourth"],
-  [5, "fifth"],
-  [6, "sixth"],
-  [7, "seventh"],
-  [8, "eighth"],
-  [9, "ninth"],
-  [10, "tenth"],
-  [11, "eleventh"],
-  [12, "twelfth"],
-  [13, "thirteenth"],
-  [14, "fourteenth"],
-  [15, "fifteenth"],
-  [16, "sixteenth"],
-  [17, "seventeenth"],
-  [18, "eighteenth"],
-  [19, "nineteenth"],
-  [20, "twentieth"],
-  [21, "twenty_first"],
-  [22, "twenty_second"],
-  [23, "twenty_third"],
-  [24, "twenty_fourth"],
-  [25, "twenty_fifth"],
-  [26, "twenty_sixth"],
-  [27, "twenty_seventh"],
-]);
+import { ordinalNumbers } from "../src/utils/ordinalNumbers.js";
 
 function getAmendmentsDoc(number) {
   const label = `amendment-${number}`;
